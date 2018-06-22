af8bf5d0a66f150485e132e055eece0a6653c7d8
vkrish011c <Vankatesk_Krishnan@cable.comcast.com>
2018-06-22 01:35:38 -0600

    Blog app up and running

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils.timezone import datetime
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length = 255,blank=False)
+    pub_date = models.DateTimeField()#default=datetime.now()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
 
 
